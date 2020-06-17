b8fb8e75bb518e801f4dd61e1e52d36f226a804c
cengizhankose <kose651@gmail.com>
2020-06-18 01:46:01 +0300

    Create Dashboard.js

---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import PokemonList from "../pokemon/PokemonList";
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col">
+          <PokemonList />
+        </div>
+      </div>
+    );
+  }
+}
