62c9465639f5ca5d1d07541df599f24b28ce38e4
cengizhankose <kose651@gmail.com>
2020-06-18 02:56:56 +0300

    Updates

---
+++ precache-manifest.1a0147727d648ccae84a489743516a4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33501b835be7aa12b5d85de847f198cb",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "06e12dd698d5653fdc16",
+    "url": "/react-pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "51c3d8380cad6f047fed",
+    "url": "/react-pokedex/static/css/main.3ba4aaaf.chunk.css"
+  },
+  {
+    "revision": "06e12dd698d5653fdc16",
+    "url": "/react-pokedex/static/js/2.1b634a50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokedex/static/js/2.1b634a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c3d8380cad6f047fed",
+    "url": "/react-pokedex/static/js/main.29aa8eb7.chunk.js"
+  },
+  {
+    "revision": "fc6afa512a7620deb760",
+    "url": "/react-pokedex/static/js/runtime-main.2ad251bb.js"
+  }
+]);
