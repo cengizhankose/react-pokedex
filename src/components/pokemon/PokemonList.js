59be6653f5802e581a87869d975c476404fabc32
cengizhankose <kose651@gmail.com>
2020-06-18 01:46:09 +0300

    Create PokemonList.js

---
+++ src/components/pokemon/PokemonList.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import PokemonCard from "./PokemonCard";
+
+export default class PokemonList extends Component {
+  state = {
+    url: "https://pokeapi.co/api/v2/pokemon?limit=964&offset=0",
+    pokemon: null,
+  };
+
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data["results"] });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.pokemon ? (
+          <div className="row">
+            {this.state.pokemon.map((pokemon) => (
+              <PokemonCard
+                key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.url}
+              />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading Pokemon...</h1>
+        )}
+      </React.Fragment>
+    );
+  }
+}
