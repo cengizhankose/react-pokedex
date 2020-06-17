cd02dcc776c8dc7433856ff845c28b79fc262c91
cengizhankose <kose651@gmail.com>
2020-06-18 01:46:07 +0300

    Create PokemonCard.js

---
+++ src/components/pokemon/PokemonCard.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+export default class PokemonCard extends Component {
+  state = {
+    name: "",
+    imageUrl: "",
+    pokemonIndex: "",
+  };
+
+  componentDidMount() {
+    const { name, url } = this.props;
+    const pokemonIndex = url.split("/")[url.split("/").length - 2];
+    const imageUrl = `https://github.com/PokeAPI/sprites/tree/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+
+    this.setState({
+      name,
+      imageUrl,
+      pokemonIndex,
+    });
+  }
+
+  render() {
+    return (
+      <div className="col-md-3 col-sm-6 mb-5">
+        <div className="card">
+          <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <div className="card-body mx-auto">
+            <h6 className="card-title">
+              {this.state.name
+                .toLowerCase()
+                .split(" ")
+                .map(
+                  (letter) =>
+                    letter.charAt(0).toUpperCase() + letter.substring(1)
+                )
+                .join(" ")}
+            </h6>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
